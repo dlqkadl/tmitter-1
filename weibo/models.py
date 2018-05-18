812a9c9c4654002551130d6854bdaa49830e4364
Gavin <Gavin-J@hotmail.com>
2018-05-19 00:04:48 +0800

    create topic & post models

--- weibo/models.py
+++ weibo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='topics')
+    created_by = models.ForeignKey(User,on_delete=models.CASCADE, related_name='topics')
+
+# class Tag(models.Model):
+#     label = models.CharField(max_length=20, unique=True)
+#     topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='tags')
+
+class Post(models.Model):
+    message = models.CharField(max_length=300)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
